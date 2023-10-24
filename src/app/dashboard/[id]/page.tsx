e3cbcf767fe69ca9432f15fcae92a99479e397f8
cRED-f <mrislam.fahim@gmail.com>
2023-10-25 04:16:53 +0600

    pdf view work continue

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,3 +1,5 @@
+import ChatBox from "@/components/ChatBox";
+import PdfViewer from "@/components/PdfViewer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
@@ -25,5 +27,24 @@ export default async function page({ params }: Params) {
     },
   });
   if (!file) notFound();
-  return <div>{id}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-h-8xl grow lg:flex xl:px-2">
+        {/*left side */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfViewer />
+          </div>
+        </div>
+
+        {/*right side */}
+        <div
+          className="flex-[0.75] shrink-0 border-t border-gray-200 lg:w-96
+        lg:border-1 lg:border-t-0"
+        >
+          <ChatBox />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/PdfViewer.tsx
@@ -0,0 +1,3 @@
+export default function PdfViewer() {
+  return <div>Pdf viewer</div>;
+}
